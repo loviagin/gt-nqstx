c47b2b79968868efae5f2613b505ec1200af785b
Ilia Loviagin <ilia.loviagin@gmail.com>
2025-08-22 01:05:24 +0300

    Changed donation

--- Sources/server/routes.swift
+++ Sources/server/routes.swift
@@ -13,6 +13,6 @@ func routes(_ app: Application) throws {
     }
     
     app.get("donate") { req async throws -> DonateDTO in
-        return DonateDTO(allowed: false)
+        return DonateDTO(allowed: true)
     }
 }
